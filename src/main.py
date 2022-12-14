88ce717b1878b38e0a8b4f502418689eeaf6a803
Justin <Justin.vz@live.nl>
2022-12-14 15:29:25 +0100

    Added a Servo class to program

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from machine import Pin, ADC, PWM
+from servo import Servo
 import utime
 
-led = PWM(Pin(25, Pin.OUT))
-sensor = ADC(Pin(28, Pin.IN))
+# Declare objects
+LED_PIN = 25
+ADC_PIN = 28
 
-print("Start")
-
+servo = Servo(25)
+led = PWM(Pin(LED_PIN, Pin.OUT))
+sensor = ADC(Pin(ADC_PIN, Pin.IN))
 
 startTime = utime.ticks_ms()
 
 while(1):
     currentTime = utime.ticks_ms() - startTime
-
     value = sensor.read_u16()
 
     if value > 10000:
@@ -20,7 +22,6 @@ while(1):
         led.duty_u16(0)
     
     voltage = (value/2**16) * 3.3
-
-    print("Spanning", round(voltage, 2))
-    # print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
+    print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
     utime.sleep_ms(10)
+
