33e9115b1e84e28ad3faaf5dcfd29ef4bcbac0c2
Jipr <64846573+Jipr@users.noreply.github.com>
2022-12-14 21:22:13 +0100

    First

--- src/main.py
+++ src/main.py
@@ -6,15 +6,25 @@ sensor = ADC(Pin(28, Pin.IN))
 
 print("Start")
 
-tresh = 1.4 #Treshold for attention algorithm  
-
+tresh = 1.4     #Treshold for attention algorithm  
+speed_t = 0     #Tail speed min,max(0,100)[%] 
+k = 0.005       #Variable for determing tail speed
 
 startTime = utime.ticks_ms()
 
-def Attention():
-     k = 2
+def Attention(voltage, speed_t):
+    if voltage >= tresh:
+        diff = voltage - tresh
+        speed_t += diff*k
+        if speed_t < 100:
+            speed_t = 100
+    else:
+        diff = tresh - voltage
+        speed_t -= diff*k
+        if speed_t < 0:
+            speed_t = 0
 
-
+    return speed_t
 
 while(1):
     currentTime = utime.ticks_ms() - startTime
@@ -29,5 +39,5 @@ while(1):
     voltage = (value/2**16) * 3.3
 
     print("Spanning", round(voltage, 2))
-    # print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
+
     utime.sleep_ms(10)
