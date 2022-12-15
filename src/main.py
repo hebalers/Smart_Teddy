dcb1e23cf3bf2f84460673de2c6d0d92e3234dcc
justinvz <justin.vz@live.nl>
2022-12-15 15:48:32 +0100

    Servo position control

--- src/main.py
+++ src/main.py
@@ -1,8 +1,36 @@
 from machine import Pin, ADC, PWM
 import utime
 
-led = PWM(Pin(25, Pin.OUT))
-sensor = ADC(Pin(28, Pin.IN))
+class Servo():
+    def __init__(self, SERVO_PIN, duty_0=int(2**16/20), duty_180=int(2*(2**16)/20)):
+        self.pwm = PWM(Pin(SERVO_PIN))
+        self.pwm.freq(50)
+
+        self.duty_0  = duty_0
+        self.duty_180 = duty_180
+
+    def posDegree(self, theta):
+        # Servo moves between a dutycycle of 1ms to 2ms with a period of 20ms (0 -> 180 degree)
+        theta_max = int(180)
+        dutyRange = self.duty_180 - self.duty_0
+        # dutyRange = (2**16/20)
+
+        dutycycle = int(theta/theta_max * dutyRange + self.duty_0)
+        self.pwm.duty_u16(dutycycle)
+        return self.pwm.duty_u16()
+
+
+FSR_PIN = 28
+
+SERVO_PIN = 15
+
+SERVO_MIN = 1800 
+SERVO_MAX = 8000
+
+deltaTime = 10 # Period of everyloop 
+
+sensor = ADC(Pin(FSR_PIN, Pin.IN))
+servo1 = Servo(SERVO_PIN=SERVO_PIN, SERVO_MIN=SERVO_MIN, SERVO_MAX=SERVO_MAX)
 
 startTime = utime.ticks_ms()
 
@@ -18,7 +46,20 @@ while(1):
     # --------        Read sensor       ---------------- 
 
     # --------        Servo control     ---------------- 
+    for i in range(180):
+        servo1.posDegree(i)
+        print(i)
+        utime.sleep_ms(10)
+    for i in range(180):
+        x = 180 - i
+        servo1.posDegree(x)
+        print(x)
+        utime.sleep_ms(10)
+
+        
+        
+        
 
     # --------        Servo control     ---------------- 
     
-    utime.sleep_ms(10)
+    utime.sleep_ms(deltaTime)
