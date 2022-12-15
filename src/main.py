71575b3410589d46eece3e64e9a9de567ec902c6
justinvz <justin.vz@live.nl>
2022-12-15 13:03:12 +0100

    New main branch

--- src/main.py
+++ src/main.py
@@ -4,23 +4,21 @@ import utime
 led = PWM(Pin(25, Pin.OUT))
 sensor = ADC(Pin(28, Pin.IN))
 
-print("Start")
-
-
 startTime = utime.ticks_ms()
 
 while(1):
     currentTime = utime.ticks_ms() - startTime
-
+    # --------        Read sensor       ---------------- 
+    """""
     value = sensor.read_u16()
-
-    if value > 10000:
-        led.duty_u16(int(value/2))
-    else:
-        led.duty_u16(0)
-    
     voltage = (value/2**16) * 3.3
-
     print("Spanning", round(voltage, 2))
-    # print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
+    print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
+    """""
+    # --------        Read sensor       ---------------- 
+
+    # --------        Servo control     ---------------- 
+
+    # --------        Servo control     ---------------- 
+    
     utime.sleep_ms(10)
