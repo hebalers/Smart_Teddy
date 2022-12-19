babb60c2ac665d31a9fd7e8527b02ab222ad203a
Jipr <64846573+Jipr@users.noreply.github.com>
2022-12-19 10:18:23 +0100

    d

--- src/main.py
+++ src/main.py
@@ -5,21 +5,23 @@ led = PWM(Pin(25, Pin.OUT))
 sensor = ADC(Pin(28, Pin.IN))
 
 #Initialize variables
-tresh = 1.4                     #Treshold for attention algorithm  
+tresh = 1                     #Treshold for attention algorithm  
 speed_t = 0                     #Tail speed min,max(0,100)[%] 
-k = 0.0005                       #Variable for determing tail speed
+k_h = 0.3                    #Variable for determing tail speed above treshold
+k_l = 0.009                     #Variable for determing tail speed below treshold
 startTime = utime.ticks_ms()    #Start time 
+
 
 
 def Attention(voltage, speed_t):
     if voltage >= tresh:
         diff = voltage - tresh
-        speed_t += diff*k
-        if speed_t < 100:
+        speed_t += diff*k_h
+        if speed_t > 100:
             speed_t = 100
     else:
         diff = tresh - voltage
-        speed_t -= diff*k
+        speed_t -= diff*k_l
         if speed_t < 0:
             speed_t = 0
 
@@ -31,9 +33,12 @@ while(1):
     
     value = sensor.read_u16()
     voltage = (value/2**16) * 3.3
+
+   
     speed_t = Attention(voltage,speed_t)
     print("Spanning", round(voltage, 1), "Tijd in ms", currentTime)
-    print("Tail speed", speed_t)
+    print("Tail speed", int(speed_t))
+
     # --------        Read sensor       ---------------- 
 
     # --------        Servo control     ---------------- 
